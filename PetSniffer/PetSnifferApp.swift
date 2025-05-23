4408cc021f6ccfede639e397c5c9117868085b14
IPeresypkina <IPeresypkina9@gmail.com>
2025-05-23 16:24:41 +0300

    Initial commit

---
+++ PetSniffer/PetSnifferApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSnifferApp.swift
+//  PetSniffer
+//
+//  Created by Ирина on 23.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetSnifferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
